9bcd8c2f32698f865b3e087b8c465f8addc6e6c2
Vinicius-Moraes20 <78623970+Vinicius-Moraes20@users.noreply.github.com>
2021-08-19 21:41:42 -0300

    Organização dos repositórios

---
+++ 2º semestre/linguagem-de-programação/aula-2/main.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h.>
+
+int main() {
+    unsigned int qntSoftware;
+    float salFixo = 1250.0, bonus;
+
+    printf("Digite a quantidade de softwares feitos pelo programador: ");
+    scanf("%d", &qntSoftware);
+    system("cls");
+
+    bonus = qntSoftware * 75.0;
+
+    printf("Salario Fixo: %.2f\n", salFixo);
+    printf("Bonus recebido: %.2f\n", bonus);
+    printf("Salario total: %.2f\n", bonus + salFixo);
+}
