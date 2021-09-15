76e44886aa005b99bb9b6bf6ce84db4b5c33e787
Vinicius-Moraes20 <78623970+Vinicius-Moraes20@users.noreply.github.com>
2021-09-15 16:44:34 -0300

    Aula 5 de IOO

---
+++ 2º semestre/introdução-orientação-objeto/aula-5/Cliente.java
@@ -0,0 +1,25 @@
+public class Cliente {
+    private String nome;
+    private String animal;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnimal(String animal) {
+        this.animal = animal;
+    }
+
+    public String getAnimal() {
+        return this.animal;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void imprimirDado() {
+        System.out.print("Nome: " + this.getNome());
+        System.out.print("Anumal: " + this.getAnimal());
+    }
+}
